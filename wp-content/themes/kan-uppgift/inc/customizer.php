775d89c26765ae795684fff447fd14ac8c6f2b68
Daniel Andersen <nirvasher@gmail.com>
2019-04-14 19:15:30 +0200

    Added boiler plate theme Underscores

---
+++ wp-content/themes/kan-uppgift/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Kan-Uppgift Theme Customizer
+ *
+ * @package Kan-Uppgift
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function kan_uppgift_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'kan_uppgift_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'kan_uppgift_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'kan_uppgift_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function kan_uppgift_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function kan_uppgift_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function kan_uppgift_customize_preview_js() {
+	wp_enqueue_script( 'kan-uppgift-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'kan_uppgift_customize_preview_js' );
